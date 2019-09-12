7ac1c5d080f7402de05290471aa3d19b4e35d69e
fmaulabux <54985543+fmaulabux@users.noreply.github.com>
2019-09-12 14:49:51 +0200

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+class UserProfile {
+    constructor() {
+    }
+}
+
+module.exports.UserProfile = UserProfile;
